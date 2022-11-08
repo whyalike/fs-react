6c36270eed83e6c30a56aaec9e9a7ee7028ee976
Alika Choo <alikachoo5@gmail.com>
2022-11-08 17:10:31 +1100

    finish exercise 1.2

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -7,18 +7,22 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
+        {props.part} {props.exercises}
       </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    </div>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
     )
 }
@@ -34,10 +38,10 @@ const Total = (props) => {
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
   const exercises1 = 10
+  const part2 = 'Using props to pass data'
   const exercises2 = 7
+  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
